fbe9be9c87a61b2078ba069c943c2cbe9c3fc14c
mickey0070a <149114701+mickey0070a@users.noreply.github.com>
2025-05-04 16:36:35 -0400

    Create awesome.nix

---
+++ modules/awesome.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+	# AwesomeWM
+	awesome
+	alsa-utils
+	curl
+	dmenu
+	mpc-cli
+	mpd
+	scrot
+	unclutter
+	xorg.xbacklight
+	xsel
+	slock
+	lua
+	lua54Packages.luarocks
+
+	# Awesome-Copycats
+	tamsyn
+	font-awesome
+	roboto
+  ];
+}
