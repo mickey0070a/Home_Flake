c4df8f543eac9974ae59d0f19a0cd3fe507427af
mickey0070a <149114701+mickey0070a@users.noreply.github.com>
2025-05-20 10:01:44 -0400

    Update rc.lua

--- modules/awesomewm/rc.lua
+++ modules/awesomewm/rc.lua
@@ -10,16 +10,16 @@
 -- If LuaRocks is installed, make sure that packages installed through it are
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 --pcall(require, "luarocks.loader")
-
+local ensure	    = require("plugins")
 local gears         = require("gears")
 local awful         = require("awful")
                       require("awful.autofocus")
 local wibox         = require("wibox")
 local beautiful     = require("beautiful")
 local naughty       = require("naughty")
-local lain          = require("lain")
+local lain          = ensure("lain")
 --local menubar       = require("menubar")
-local freedesktop   = require("freedesktop")
+local freedesktop   = ensure("freedesktop", "awesome-freedesktop")
 local hotkeys_popup = require("awful.hotkeys_popup")
                       require("awful.hotkeys_popup.keys")
 local mytable       = awful.util.table or gears.table -- 4.{0,1} compatibility
