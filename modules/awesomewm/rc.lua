8ce42bfaf822ec5aea95f88c45f3b60d6225c333
mickey0070a <0mike0hall0@gmail.com>
2025-05-25 18:39:26 -0400

    Updated rc.lua got lain recognized. Freedesktop requires lua 5.3 or newer working on that

--- modules/awesomewm/rc.lua
+++ modules/awesomewm/rc.lua
@@ -9,13 +9,19 @@
 
 -- If LuaRocks is installed, make sure that packages installed through it are
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
+--pcall(require, "luarocks.loader")
 
 local home = os.getenv("HOME")
 package.path = package.path
-.. ";" .. home .. "/.luarocks/share/lua/5.4/?.lua"
-.. ";" .. home .. "/.luarocks/lib/lua/5.4/?.so"
+  .. ";" .. home .. "/.luarocks/share/lua/5.2/?.lua"
+  .. ";" .. home .. "/.luarocks/share/lua/5.2/?/init.lua"
+package.cpath = package.cpath
+  .. ";" .. home .. "/.luarocks/lib/luarocks/rocks-5.2/?.so"
+--local home = os.getenv("HOME")
+--package.path = package.path
+--.. ";" .. home .. "/.luarocks/share/lua/5.4/?.lua"
+--.. ";" .. home .. "/.luarocks/lib/lua/5.4/?.so"
 
-pcall(require, "luarocks.loader")
 local ensure	    = require("plugins")
 local gears         = require("gears")
 local awful         = require("awful")
@@ -23,9 +29,9 @@ local awful         = require("awful")
 local wibox         = require("wibox")
 local beautiful     = require("beautiful")
 local naughty       = require("naughty")
-local lain          = ensure("lain")
+local lain          = require("lain")
 --local menubar       = require("menubar")
-local freedesktop   = ensure("freedesktop", "awesome-freedesktop")
+local freedesktop   = require("freedesktop")
 local hotkeys_popup = require("awful.hotkeys_popup")
                       require("awful.hotkeys_popup.keys")
 local mytable       = awful.util.table or gears.table -- 4.{0,1} compatibility
