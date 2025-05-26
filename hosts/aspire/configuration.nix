436c0be9a7d9f6fc610dc5ea0656b0919366bda4
mickey0070a <149114701+mickey0070a@users.noreply.github.com>
2025-05-26 14:25:18 -0400

    Update configuration.nix

--- hosts/aspire/configuration.nix
+++ hosts/aspire/configuration.nix
@@ -72,6 +72,9 @@
   gh
   ];
 
+  # Enable services like Moonraker (for API)
+  services.moonraker.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
