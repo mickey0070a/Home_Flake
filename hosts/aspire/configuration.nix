591da12687a7b161470af190ee663476ccef1cfe
mickey0070a <0mike0hall0@gmail.com>
2025-06-10 18:26:59 -0400

    updated server hostname

--- hosts/aspire/configuration.nix
+++ hosts/aspire/configuration.nix
@@ -15,7 +15,7 @@
   boot.loader.grub.device = "/dev/mmcblk0";
   boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "nixos-ender3-server"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
