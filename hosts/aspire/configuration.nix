a6d7465b00ba4b233b7020374e3e0afabd0856ae
mickey0070a <149114701+mickey0070a@users.noreply.github.com>
2025-06-13 19:11:22 -0400

    Update configuration.nix

--- hosts/aspire/configuration.nix
+++ hosts/aspire/configuration.nix
@@ -82,13 +82,13 @@
 
   services.klipper = {
     enable = true;
-    firmwares = {
-      mcu = {
-        enable = true;
-        configFile = /home/ender3/config;
-        serial = "/dev/serial/by-id/usb-1a86_USB_Serial-if00-port0";
-      };
-    };
+    #firmwares = {
+      #mcu = {
+        #enable = true;
+        #configFile = /home/ender3/config;
+        #serial = "/dev/serial/by-id/usb-1a86_USB_Serial-if00-port0";
+      #};
+    #};
   };
 
   # Enable services like Moonraker (for API)
