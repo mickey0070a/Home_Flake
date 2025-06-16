d800ca738c85b52b032cd72a95a502be244f3560
mickey0070a <149114701+mickey0070a@users.noreply.github.com>
2025-06-16 12:08:50 -0400

    Update cerih.nix

--- profiles/cerih/cerih.nix
+++ profiles/cerih/cerih.nix
@@ -16,14 +16,26 @@
   programs.zsh.enable = true;
   programs.zsh.oh-my-zsh.enable = true;
 
-  #xsession.enable = true;
-  #xsession.windowManager.awesome.enable = true;
+  nixpkgs.overlays = [
+	(self: super: {
+		awesome = super.awesome.override {
+			lua = self.lua53Packages.lua;
+		};
+	})
+  ]; 
 
-    #home.activation.copyAwesomeConfig = lib.hm.dag.entryAfter #["writeBoundary"] ''
-    #rm -rf ~/.config/awesome
-    #cp -r ${../modules/awesomewm} ~/.config/awesome
-    #'';
-
+  xsession ={
+	enable = true;
+        windowManager.awesome = {
+		enable = true;
+                #luaModules = with pkgs.lua53Packages; [
+                     #luarocks
+                     #luadbi-mysql
+                     #vicious
+                #];
+               # package = 54awesome;
+	};
+  };
   xdg.configFile."awesome".source = ./awesomewm;
 
   nixpkgs.config.allowUnfree = true;
