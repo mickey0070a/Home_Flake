f1a79336348c85e327a5deda00e6d27ed0bd514b
Mickey0070a <0mike0hall0@gmail.com>
2025-06-21 19:52:41 -0400

    file share working

--- modules/samba.nix
+++ modules/samba.nix
@@ -2,37 +2,37 @@
 
 {
 
- {
-  fileSystems."/mnt/data" = {
+  fileSystems."/mnt/network" = {
     device = "/dev/disk/by-uuid/a195148e-9531-4520-b483-0f429fb5a8bb";  # or use the actual UUID
     fsType = "ext4";
     options = [ "defaults" ];
   };
-}
 
  environment.systemPackages = with pkgs; [
         samba
   ];
+
   services.samba = {
   enable = true;
   openFirewall = true;
   settings = {
-    #global = {
-      #workgroup = "WORKGROUP";
-      #"disable netbios" = "yes";
-      #"unix extensions" = "yes";
-      #"map to guest" = "Bad User";
-      #server.string = "Nix file server";
+    global = {
+	workgroup = "WORKGROUP";
+	"netbios name" = "Nixos";
+	"server string" = "NixOS Home Server";
+	security = "user";
+	"map to guest" = "Bad User";
+	"guest account" = "server";
     };
     public = {
       path = "/mnt/network";
-      browseable = yes;
-      "guest ok" = yes;
-      "read only" = no;
+      browseable = "yes";
+      "guest ok" = "yes";
+      "read only" = "no";
       "create mask" = "0664";
       "directory mask" = "0775";
       "force user" = "server";
-      "force group" = server;
+      "force group" = "server";
     };
   };
 };
