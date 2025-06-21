85a28316c3e7ae0056e4e0d3d1fb01ef9543beef
mickey0070a <0mike0hall0@gmail.com>
2025-06-21 11:09:02 -0400

    updated flake for file sharing

--- modules/samba.nix
+++ modules/samba.nix
@@ -1,6 +1,15 @@
 { pkgs, ... }:
 
 {
+
+ {
+  fileSystems."/mnt/data" = {
+    device = "/dev/disk/by-uuid/a195148e-9531-4520-b483-0f429fb5a8bb";  # or use the actual UUID
+    fsType = "ext4";
+    options = [ "defaults" ];
+  };
+}
+
  environment.systemPackages = with pkgs; [
         samba
   ];
@@ -8,22 +17,22 @@
   enable = true;
   openFirewall = true;
   settings = {
-    global = {
-      workgroup = "WORKGROUP";
-      "disable netbios" = "yes";
-      "unix extensions" = "yes";
-      "map to guest" = "Bad User";
+    #global = {
+      #workgroup = "WORKGROUP";
+      #"disable netbios" = "yes";
+      #"unix extensions" = "yes";
+      #"map to guest" = "Bad User";
       #server.string = "Nix file server";
     };
     public = {
-      path = "/srv/public";
-      browseable = "yes";
-      "guest ok" = "yes";
-      "read only" = "no";
+      path = "/mnt/network";
+      browseable = yes;
+      "guest ok" = yes;
+      "read only" = no;
       "create mask" = "0664";
       "directory mask" = "0775";
       "force user" = "server";
-      "force group" = "server";
+      "force group" = server;
     };
   };
 };
