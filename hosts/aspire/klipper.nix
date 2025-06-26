c91e77cc303d25845c3346b2e6b489cfef4a5e85
mickey0070a <149114701+mickey0070a@users.noreply.github.com>
2025-06-26 12:18:19 -0400

    Create klipper.nix

---
+++ hosts/aspire/klipper.nix
@@ -0,0 +1,14 @@
+ environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+  git
+  gh
+  usbutils
+  klipper
+  klipper-flash
+  klipper-firmware
+  klipper-genconf
+  klipper-estimator
+  mainsail
+  moonraker
+  ];
