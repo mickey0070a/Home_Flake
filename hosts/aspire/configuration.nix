14a46bcab436dbdc81dec3dfe3ba9a419dfadd20
mickey0070a <149114701+mickey0070a@users.noreply.github.com>
2025-06-27 20:15:31 -0400

    Update configuration.nix

--- hosts/aspire/configuration.nix
+++ hosts/aspire/configuration.nix
@@ -119,7 +119,7 @@
     enable = true;
     address = "0.0.0.0";
     allowSystemControl = true;
-    klipperSocket = "/tmp/klipper.sock";
+    klipperSocket = "~/server/klipper.sock";
     settings = {
       authorization = {
         force_logins = true;
