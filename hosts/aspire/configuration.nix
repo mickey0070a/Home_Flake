41a12137bccd60ddae1fb734f0be03b69fa071e2
mickey0070a <149114701+mickey0070a@users.noreply.github.com>
2025-06-29 16:59:49 -0400

    Update configuration.nix

--- hosts/aspire/configuration.nix
+++ hosts/aspire/configuration.nix
@@ -105,7 +105,7 @@
  services.klipper = {
     enable = true;
     configFile = ./printer.cfg; 
-    apiSocket = "/tmp/printer";
+    apiSocket = /tmp/printer;
     #octoprintIntegration = true;
     logFile = "/tmp/klippy.log";
     #firmwares = {
