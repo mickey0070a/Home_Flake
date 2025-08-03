2ac960eb3c710209c65a86043b05722b25e7046c
mickey0070a <149114701+mickey0070a@users.noreply.github.com>
2025-08-03 19:09:04 -0400

    Update configuration.nix

--- hosts/MikeDesktop/configuration.nix
+++ hosts/MikeDesktop/configuration.nix
@@ -10,6 +10,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ../../modules/samba.nix
+      ../../modules/Trilium-Server.nix
     ];
   
   # Bootloader.
