8f3fe9ee8118a46a11f46d143a9bf72745cb4180
mickey0070a <149114701+mickey0070a@users.noreply.github.com>
2025-08-03 17:51:49 -0400

    Create Trilium-Server.nix

---
+++ modules/Trilium-Server.nix
@@ -0,0 +1,23 @@
+{ config, inputs, pkgs, unstable, ... }:
+
+{
+  _module.args.unstable = import inputs.nixpkgs-unstable {
+	inherit (pkgs.stdenv.hostPlatform) system;
+	inherit (config.nixpkgs) config;
+  };
+  home.packages = [
+	unstable.trilium-next-server
+  ];
+  services.nginx.enable = true;
+  services.ngix.virtualHosts."trilium.Hall.com" = {
+    forceSSL = true;
+    enableACME = true;
+    locations."/" = {
+      proxyPass = "http://localhost:8080";
+      proxyWebsockets = true;
+      basicAuthFile = ./trilium.htpasswd;
+    };
+  };
+  security.acme.acceptTerms = true;
+  security.acme.email = "0mike0hall0@gmail.com";
+}
