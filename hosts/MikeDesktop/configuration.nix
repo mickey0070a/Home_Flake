4924681971c92b426af5b4f79d88ad6b40a47780
mickey0070a <149114701+mickey0070a@users.noreply.github.com>
2025-08-06 18:08:26 -0400

    Update configuration.nix

--- hosts/MikeDesktop/configuration.nix
+++ hosts/MikeDesktop/configuration.nix
@@ -12,6 +12,8 @@
       ../../modules/samba.nix
       ../../modules/Trilium-Server.nix
       ../../modules/3dprinting.nix
+      ../../profiles/users.nix
+      ../../profiles/octoprint.nix
     ];
   
   # Bootloader.
@@ -117,27 +119,6 @@
 
   users.groups.server = {};
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.michaelh = {
-    isNormalUser = true;
-    description = "Michael Hall";
-    extraGroups = [ "networkmanager" "wheel" "storage" "server" ];
-    hashedPassword = "$6$bSnWiQcaHCpYd2qB$8dFX7vGp1TnKm3H/e/NFw/hvOUQ3FooLphnNVZrW4kEwpni910oeSfTo9FZmD5JIHADWV7sAjnYHFZ1/W2feV/";
-    packages = with pkgs; [
-    #  thunderbird
-    ];
-  };
-
-    users.users.cerih = {
-    isNormalUser = true;
-    description = "Ceri Hall";
-    extraGroups = [ "networkmanager" "wheel" "storage" ];
-    hashedPassword = "$6$Vhoyi8jFQBEzuGZV$IV7cElFLzzVUn8thZ9jrREXssDg7yeE/4U8GyEBBaYPGpqfZZVm7Duczl3gCmqMy3nk8EtXjXRvrCgBiwUzRb0";
-    packages = with pkgs; [
-    #  thunderbird
-    ];
-  };
-  
   users.users.server = {
     isNormalUser = true;
     description = "server";
