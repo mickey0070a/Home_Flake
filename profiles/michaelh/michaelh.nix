cb7c1e0bb385a426216cdb6da7ca4130e9674b81
mickey0070a <149114701+mickey0070a@users.noreply.github.com>
2025-08-07 19:27:19 -0400

    Update michaelh.nix

--- profiles/michaelh/michaelh.nix
+++ profiles/michaelh/michaelh.nix
@@ -16,43 +16,32 @@
   
   programs.zsh.enable = true;
   programs.zsh.oh-my-zsh.enable = true;
-  
-  #xsession.enable = true;
-  #xsession.windowManager.awesome.enable = true;
 
-  nixpkgs.overlays = [
-	(self: super: {
-		awesome = super.awesome.override {
-			lua = self.lua53Packages.lua;
-		};
-	})
-  ]; 
-
-  xsession ={
-	enable = true;
+  let
+	self = import <nixpkgs>  {};
+	super = import <nixpkgs> { 
+		overlays = [
+			(self: super: {
+				awesome = super.awesome.override {
+				lua = self.lua53Packages.lua;
+				};
+			})
+  		];
+	};
+  in
+  {
+  	xsession ={
+		enable = true;
         windowManager.awesome = {
 		enable = true;
-                #luaModules = with pkgs.lua53Packages; [
-                     #luarocks
-                     #luadbi-mysql
-                     #vicious
-                #];
-               # package = 54awesome;
-	};
+		package = super.awesome;
+        };
+  	};
   };
-
-  
-    #home.activation.copyAwesomeConfig = lib.hm.dag.entryAfter #["writeBoundary"] ''
-    #rm -rf ~/.config/awesome
-    #cp -r ${../modules/awesomewm} ~/.config/awesome
-    #'';
 
   xdg.configFile."awesome".source = ./awesomewm;
      
   nixpkgs.config.allowUnfree = true;
-
-   #home.packages = with pkgs; [
-  #];
 
    programs = {
 	  git = {
