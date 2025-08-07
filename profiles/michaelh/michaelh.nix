dc2bde3a8c2fd0a8f43ee7ee74c369be052e419a
mickey0070a <149114701+mickey0070a@users.noreply.github.com>
2025-08-07 19:37:27 -0400

    Update michaelh.nix

--- profiles/michaelh/michaelh.nix
+++ profiles/michaelh/michaelh.nix
@@ -17,28 +17,22 @@
   programs.zsh.enable = true;
   programs.zsh.oh-my-zsh.enable = true;
 
-  let
-	self = import <nixpkgs>  {};
-	super = import <nixpkgs> { 
-		overlays = [
-			(self: super: {
-				awesome = super.awesome.override {
-				lua = self.lua53Packages.lua;
-				};
-			})
-  		];
-	};
-  in
-  {
-  	xsession ={
+  xsession ={
+	enable = true;
+    windowManager.awesome = {
 		enable = true;
-        windowManager.awesome = {
-		enable = true;
-		package = super.awesome;
-        };
-  	};
+		package =  (import <nixpkgs> { 
+			overlays = [
+				(self: super: {
+					awesome = super.awesome.override {
+						lua = self.lua53Packages.lua;
+					};
+				})
+  			];
+		}).awesome;
+    };
   };
-
+ 
   xdg.configFile."awesome".source = ./awesomewm;
      
   nixpkgs.config.allowUnfree = true;
