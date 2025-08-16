4a89977cf0f3159a4e3afdfe5f24238ef5b359ee
mickey0070a <149114701+mickey0070a@users.noreply.github.com>
2025-08-16 18:48:11 -0400

    Update 3dprinting.nix

--- modules/3dprinting.nix
+++ modules/3dprinting.nix
@@ -34,7 +34,7 @@
 
  services.klipper = {
     enable = true;
-    configFile = "home/flake/Home_Flake/modules/printer.cfg" ; 
+    configFile = "/home/flake/Home_Flake/modules/printer.cfg" ; 
    # apiSocket = "/tmp/printer.ser";
     inputTTY = "/tmp/printer";
     octoprintIntegration = true;
