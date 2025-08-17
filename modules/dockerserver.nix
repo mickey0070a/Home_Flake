9500a6e1503455b8cf541ee9a80f29c7b8b016e5
mickey0070a <149114701+mickey0070a@users.noreply.github.com>
2025-08-17 16:26:16 -0400

    Update dockerserver.nix

--- modules/dockerserver.nix
+++ modules/dockerserver.nix
@@ -15,7 +15,7 @@
       ports = [ "127.0.0.1:8080:8080" ]; # Expose only to localhost
       volumes = [ "trilium-data:/home/node/trilium-data" ];
       environment = {
-        TAILSCALE_AUTHKEY = "${config.services.tailscale.authKey}"; # Use the global auth key
+        TAILSCALE_AUTHKEY = "tskey-auth-kwe8SMWkSX11CNTRL-pFb7GKB1Xu7zT8d6ARpLv7WZzpuBCBNYd"; # Use the global auth key
       };
       entrypoint = "sh -c 'tailscaled & sleep 3 && tailscale up --authkey=$TAILSCALE_AUTHKEY && trilium'"; # Start Tailscale and then Trilium
     };
