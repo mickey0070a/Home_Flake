fc1648bf48f44850177b19140332193fc41888ef
mickey0070a <149114701+mickey0070a@users.noreply.github.com>
2025-08-20 19:21:30 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
   {
       nixosConfigurations.getac = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          specialArgs = { inherit inputs };
+          specialArgs = { inherit inputs; };
           modules = [
             ./hosts/getac/configuration.nix
             home-manager.nixosModules.home-manager {
@@ -23,13 +23,13 @@
               home-manager.useUserPackages = true;
               home-manager.users.michaelh = import ./profiles/michaelh/michaelh.nix;
               home-manager.users.cerih = import ./profiles/cerih/cerih.nix;
-              home-manager.extraSpecialArgs = { inherit inputs };
+              home-manager.extraSpecialArgs = { inherit inputs; };
             }
           ];
       };
         nixosConfigurations.aspire = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          specialArgs = { inherit inputs };
+          specialArgs = { inherit inputs; };
           modules = [
             ./hosts/aspire/configuration.nix
             home-manager.nixosModules.home-manager {
@@ -37,13 +37,13 @@
               home-manager.useUserPackages = true;
               home-manager.users.michaelh = import ./profiles/michaelh/michaelh.nix;
               home-manager.users.cerih = import ./profiles/cerih/cerih.nix;
-              home-manager.extraSpecialArgs = { inherit inputs };
+              home-manager.extraSpecialArgs = { inherit inputs; };
           }
         ];
       };
         nixosConfigurations.MikeDesktop = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          specialArgs = { inherit inputs };
+          specialArgs = { inherit inputs; };
           modules = [
             ./hosts/MikeDesktop/configuration.nix
             home-manager.nixosModules.home-manager {
@@ -51,7 +51,7 @@
               home-manager.useUserPackages = true;
               home-manager.users.michaelh = import ./profiles/michaelh/michaelh.nix;
               home-manager.users.cerih = import ./profiles/cerih/cerih.nix;
-              home-manager.extraSpecialArgs = { inherit inputs };
+              home-manager.extraSpecialArgs = { inherit inputs; };
             }
           ];
       };
