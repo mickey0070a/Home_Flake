d8613f920848db93f9517699429e6f390c43dce5
mickey0070a <149114701+mickey0070a@users.noreply.github.com>
2025-11-28 17:05:55 -0500

    Update engineering.nix

--- modules/engineering.nix
+++ modules/engineering.nix
@@ -11,15 +11,15 @@
 	freecad
 	super-slicer
 	qgis
-	python312Full
-	python312Packages.networkx
-	python312Packages.openpyxl
-	python312Packages.numpy
-	python312Packages.scipy
-	python312Packages.matplotlib
-	python312Packages.requests
-	python312Packages.pyserial
-	python312Packages.pip
-	python312Packages.lxml
+	python311Full
+	python311Packages.networkx
+	python311Packages.openpyxl
+	python311Packages.numpy
+	python311Packages.scipy
+	python311Packages.matplotlib
+	python311Packages.requests
+	python311Packages.pyserial
+	python311Packages.pip
+	python311Packages.lxml
   ];
 }
