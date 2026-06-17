bc910dabdf1d4e2bb64c3ca1ff7bbe17d7892fd7
mickey0070a <149114701+mickey0070a@users.noreply.github.com>
2026-06-17 15:54:13 -0400

    Replace freecad with freecad-fhs in home.packages

--- modules/engineering.nix
+++ modules/engineering.nix
@@ -1,27 +1,36 @@
 { pkgs, ... }:
 
+let
+  freecad-fhs = pkgs.buildFHSUserEnv {
+    name = "freecad";
+    targetPkgs = pkgs: with pkgs; [
+      freecad
+      python3
+      python3Packages.pip
+      mesa
+      libGL
+    ];
+    runScript = "${pkgs.freecad}/bin/freecad";
+    profile = ''
+      export PIP_PREFIX=$HOME/.local/pip-freecad
+      export PYTHONPATH="$PIP_PREFIX/lib/python3.11/site-packages:$PYTHONPATH"
+      export PATH="$PIP_PREFIX/bin:$PATH"
+      mkdir -p "$PIP_PREFIX"
+      unset SOURCE_DATE_EPOCH
+    '';
+  };
+in
 {
   home.packages = with pkgs; [
-	librecad
-	qalculate-gtk
-	gmsh
-	calculix-ccx
-	inkscape-with-extensions
-	gimp
-	freecad
-	super-slicer
- orca-slicer
-	qgis
-	python311Full
-	python311Packages.networkx
-	python311Packages.openpyxl
-	python311Packages.numpy
-	python311Packages.scipy
-	python311Packages.matplotlib
-	python311Packages.requests
-	python311Packages.pyserial
-	python311Packages.pip
-	python311Packages.lxml
-	python311Packages.ezdxf
+    librecad
+    qalculate-gtk
+    gmsh
+    calculix-ccx
+    inkscape-with-extensions
+    gimp
+    freecad-fhs          # <-- replaced freecad with freecad-fhs
+    super-slicer
+    orca-slicer
+    qgis
   ];
 }
