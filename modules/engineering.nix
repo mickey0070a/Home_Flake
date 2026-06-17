a9bdcf0fe6dbd20cfa1d3abe06477c1fb78d379b
mickey0070a <149114701+mickey0070a@users.noreply.github.com>
2026-06-17 15:57:41 -0400

    Replace freecad-fhs with freecad-with-pip

--- modules/engineering.nix
+++ modules/engineering.nix
@@ -1,24 +1,14 @@
 { pkgs, ... }:
 
 let
-  freecad-fhs = pkgs.buildFHSUserEnv {
-    name = "freecad";
-    targetPkgs = pkgs: with pkgs; [
-      freecad
-      python3
-      python3Packages.pip
-      mesa
-      libGL
-    ];
-    runScript = "${pkgs.freecad}/bin/freecad";
-    profile = ''
-      export PIP_PREFIX=$HOME/.local/pip-freecad
-      export PYTHONPATH="$PIP_PREFIX/lib/python3.11/site-packages:$PYTHONPATH"
-      export PATH="$PIP_PREFIX/bin:$PATH"
-      mkdir -p "$PIP_PREFIX"
-      unset SOURCE_DATE_EPOCH
-    '';
-  };
+  freecad-with-pip = pkgs.writeShellScriptBin "freecad" ''
+    export PIP_PREFIX=$HOME/.local/pip-freecad
+    export PYTHONPATH="$PIP_PREFIX/lib/python3.11/site-packages:$PYTHONPATH"
+    export PATH="$PIP_PREFIX/bin:$PATH"
+    mkdir -p "$PIP_PREFIX"
+    
+    exec ${pkgs.steam-run}/bin/steam-run ${pkgs.freecad}/bin/freecad "$@"
+  '';
 in
 {
   home.packages = with pkgs; [
@@ -28,9 +18,10 @@ in
     calculix-ccx
     inkscape-with-extensions
     gimp
-    freecad-fhs          # <-- replaced freecad with freecad-fhs
+    freecad-with-pip
     super-slicer
     orca-slicer
     qgis
+    steam-run  # Make sure this is available
   ];
 }
