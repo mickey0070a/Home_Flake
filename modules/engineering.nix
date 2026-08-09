7c46d837249fabe1532b4f1b602583ae3e3204c5
mickey0070a <149114701+mickey0070a@users.noreply.github.com>
2026-08-08 22:42:32 -0400

    Refactor FreeCAD script for pip installation
    Updated FreeCAD script to use PYTHONUSERBASE for pip installation.

--- modules/engineering.nix
+++ modules/engineering.nix
@@ -1,14 +1,17 @@
 { pkgs, ... }:
 
 let
-  freecad-with-pip = pkgs.writeShellScriptBin "freecad" ''
-    export PIP_PREFIX=$HOME/.local/pip-freecad
-    export PYTHONPATH="$PIP_PREFIX/lib/python3.13/site-packages:$PYTHONPATH"
-    export PATH="$PIP_PREFIX/bin:$PATH"
-    mkdir -p "$PIP_PREFIX"
-    
-    exec ${pkgs.steam-run}/bin/steam-run ${pkgs.freecad}/bin/freecad "$@"
-  '';
+freecad-with-pip = pkgs.writeShellScriptBin "freecad" ''
+  export PYTHONUSERBASE="$HOME/.local"
+
+  ${pkgs.python313Packages.pip}/bin/pip install \
+    --target "$PYTHONUSERBASE/lib/python3.13/site-packages" \
+    pip setuptools wheel >/dev/null 2>&1 || true
+
+  export PYTHONPATH="$PYTHONUSERBASE/lib/python3.13/site-packages:$PYTHONPATH"
+
+  exec ${pkgs.steam-run}/bin/steam-run ${pkgs.freecad}/bin/freecad "$@"
+'';
 in
 {
   home.packages = with pkgs; [
