95393603b5e8f01241c26fdb27beeff9f2516eab
mickey0070a <149114701+mickey0070a@users.noreply.github.com>
2026-08-09 20:37:31 -0400

    Add ondsel-server Docker container to module

--- modules/dockerserver.nix
+++ modules/dockerserver.nix
@@ -8,6 +8,7 @@
 
   # Ensure Docker is enabled
   virtualisation.docker.enable = true;
+  virtualisation.oci-containers.backend = "docker";
 
   virtualisation.oci-containers.containers = {
     trilium = {
@@ -18,23 +19,27 @@
         TAILSCALE_AUTHKEY = "tskey-auth-kwe8SMWkSX11CNTRL-pFb7GKB1Xu7zT8d6ARpLv7WZzpuBCBNYd"; # Use the global auth key
       };
       entrypoint = "sh -c 'tailscaled & sleep 3 && tailscale up --authkey=$TAILSCALE_AUTHKEY && trilium'"; # Start Tailscale and then Trilium
+      autoStart = true;
+    };
+
+    ondsel-server = {
+      image = "ghcr.io/ondsel/ondsel-server:latest";
+      ports = [ "127.0.0.1:3000:3000" ];
+      volumes = [ "/var/lib/ondsel:/data" ];
+      environment = {
+        TZ = "America/New_York";
+      };
+      autoStart = true;
     };
   };
 
   # You might need to set up Docker volumes manually
-  #system.activationScripts.createDockerVolumes.tex#t = ''
-#    docker volume create trilium-data || true
- # '';
-
-# You might need to set up Docker volumes manually
-#system.activationScripts.createDockerVolumes.text = ''
-  #docker volume create trilium-data || true
-  #docker volume create tailscale-state || true
-#'';
-
-# Define a systemd service for the tailscale container
+  #system.activationScripts.createDockerVolumes.text = ''
+  #  docker volume create trilium-data || true
+  #  docker volume create tailscale-state || true
+  #'';
 
   # Optional firewall config
-  networking.firewall.allowedTCPPorts = [ 80 443 8080 ]; # Or 8080 if you want to expose Trilium directly
+  networking.firewall.allowedTCPPorts = [ 80 443 3000 8080 ]; # Added 3000 for Ondsel
 
 }
