b2527d2006ba84c08c3a0f3e435cd2af242ec7c3
mickey0070a <149114701+mickey0070a@users.noreply.github.com>
2026-09-20 19:08:49 -0400

    Disable proxy settings in nginx.nix
    Comment out the proxy configuration for the Nginx server.

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -74,20 +74,20 @@ services.nginx = {
     "onsdel.nixserver.tailnet" = {
       basicAuthFile = "/etc/nginx/htpasswd";
 
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:3001/";
+      #locations."/" = {
+       # proxyPass = "http://127.0.0.1:3001/";
 
-        extraConfig = ''
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
+        #extraConfig = ''
+         # proxy_set_header Host $host;
+          #proxy_set_header X-Real-IP $remote_addr;
+          #proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          #proxy_set_header X-Forwarded-Proto $scheme;
 
-          proxy_http_version 1.1;
-          proxy_set_header Upgrade $http_upgrade;
-          proxy_set_header Connection $connection_upgrade;
-        '';
-      };
+          #proxy_http_version 1.1;
+          #proxy_set_header Upgrade $http_upgrade;
+          #proxy_set_header Connection $connection_upgrade;
+        #'';
+      #};
     };
   };
 };
