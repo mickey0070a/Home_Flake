096dda45460dee774ec9da4991e4bf64c9687897
mickey0070a <149114701+mickey0070a@users.noreply.github.com>
2026-09-23 19:34:34 -0400

    Update nginx.nix

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -2,23 +2,49 @@
 
 {
   services.nginx = {
-    enable = true;
-    recommendedProxySettings = true;
+  enable = true;
 
-    virtualHosts = {
-      "_" = {
-        basicAuthFile = "/etc/nginx/htpasswd";
+  virtualHosts."_" = {
+    listen = [
+      {
+        addr = "0.0.0.0";
+        port = 80;
+      }
+    ];
 
-        locations."/" = {
-          return = "404";
-        };
-      };
+    serverName = "_";
+
+    basicAuthFile = "/etc/nginx/htpasswd";
+
+    locations."/remote-access/" = {
+      proxyPass = "http://127.0.0.1:8787";
+
+      extraConfig = ''
+        proxy_http_version 1.1;
+
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+      '';
     };
 
-    appendHttpConfig = ''
-      include /var/lib/remote-access/routes/*.conf;
+    # The ONLY location /
+    locations."/" = {
+      extraConfig = ''
+        include /var/lib/remote-access/root-route.conf;
+      '';
+    };
+
+    # OctoPrint and Trilium are generated dynamically by the controller.
+    extraConfig = ''
+      include /var/lib/remote-access/routes.conf;
     '';
   };
+};
 
   networking.firewall.allowedTCPPorts = [ 80 ];
 }
